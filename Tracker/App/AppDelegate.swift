a09fdd48c2f6bd108def5ab76f67c2ac308de743
Kovac <biu.jay1@yandex.ru>
2026-02-18 14:14:06 +0300

    build: Убрано импорт библиотеки CoreData из файла, в котором не используется

--- Tracker/App/AppDelegate.swift
+++ Tracker/App/AppDelegate.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -31,7 +30,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
-    
-    
 }
 
